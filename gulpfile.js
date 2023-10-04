a68de5003062c46b540348ef05b275348fe2e502
“Aline <hy.aline@outlook.com>
2023-10-04 20:46:24 +0200

    did a lot of articles -04/10/20230

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@
 /* Basic comment */
 
 // .bandeau-footer-body2021v2
-var classParent = '.faq_pdp_rituel_2023';
+var classParent = '.rituel_sommeil_article_2023';
 
 const {
     series,
--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,6 @@ function init() {
     jschecker = checkLoaded();
     if (jschecker) {
         console.log('Script started');
-        // =require specific/2023/faq_pdp_rituel.js
     } else {
         setTimeout(init);
     }
