f00a90bf1b7e86be389a99c6e972b597c0557ac0
“Aline <hy.aline@outlook.com>
2024-02-01 09:51:23 +0100

    1/02/2024 -refonte assets vrac homepromo pour tous les pays

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@
 /* Basic comment */
 
 // .bandeau-footer-body2021v2
-var classParent = '.offre_tisaniere_hero_2024';
+var classParent = '.gen_recharge_the_homepromo_2024';
 
 const {
     series,
