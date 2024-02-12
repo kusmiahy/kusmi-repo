bec3fe9303bcef214ab46a690d967f52d830769c
“Aline <hy.aline@outlook.com>
2024-02-12 11:59:58 +0100

    12/02/2024 -avancement politique de confidentialite

---
+++ src/js/specific/2024/politique_de_confidentialite.js
@@ -0,0 +1,16 @@
+const allQuestions = document.querySelectorAll('.single__title-container');
+
+
+
+/**
+ * Quand une question est cliquée, la réponse apparait et si on reclique elle disparait
+ */
+
+document.addEventListener("DOMContentLoaded", function () {
+    allQuestions.forEach((question) => {
+        question.addEventListener('click', () => {
+            console.log("click");
+            !question.classList.contains('single__title-container--toggled') ? question.classList.add('single__title-container--toggled') : question.classList.remove('single__title-container--toggled');
+        })
+    })
+})
