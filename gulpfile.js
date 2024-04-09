689b11b696c5269240764fc91f76f52bddb94494
kusmiahy <a.hy@orientis.fr>
2024-04-09 17:24:24 +0200

    ajout de plein de trucs asset ducasse asset vrac multi recettes + assets lapsang souchong + contenu enrichi de pdp pour les 6S et pour coffret thé glacé

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@
 /* Basic comment */
 
 // .bandeau-footer-body2021v2
-var classParent = '.blog_article_2024';
+var classParent = '.the_glace_2024';
 
 const {
     series,
--- src/js/specific/2024/simple_carousel.js
+++ src/js/specific/2024/simple_carousel.js
@@ -1,21 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
-    const splide = new Splide('.splide--pdp-bundle-rituel', {
-        type: 'loop',
-            perPage: 4,
-            drag: false,
-            arrows: false,
-            pagination: false,
-            breakpoints: {
-                767: {
-                    perPage: 1,
-                    arrows: true,
-                    autoplay: true,
-                    speed: 1000,
-                    easing: 'ease',
-                    drag: true,
-                },
-            }
-        });
-    splide.mount();
+    if(document.querySelector('.splide--pdp')) {
+        const splide = new Splide('.splide--pdp', {
+            type: 'loop',
+                perPage: 4,
+                drag: false,
+                arrows: false,
+                pagination: false,
+                breakpoints: {
+                    767: {
+                        perPage: 1,
+                        arrows: true,
+                        autoplay: true,
+                        speed: 1000,
+                        easing: 'ease',
+                        drag: true,
+                    },
+                }
+            });
+        splide.mount();
+    }
 })
