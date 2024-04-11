3100bc5c19278d6ae27f27ab995f6c319f93a45e
kusmiahy <a.hy@orientis.fr>
2024-04-11 17:31:09 +0200

    fdm tablette et desktop hero en cours

--- src/js/specific/2024/carousel_fdm.js
+++ src/js/specific/2024/carousel_fdm.js
@@ -7,15 +7,30 @@ document.addEventListener("DOMContentLoaded", function () {
             speed: 1000,
             easing: 'ease',
             perPage: 6,
+            perMove: 1,
+            pagination: false,
+            gap: '.5rem',
                 breakpoints: {
                     767: {
-                        perPage: 1,
+                        perPage: 2,
+                        gap: '.5rem',
+                    },
+                    1024: {
+                        perPage: 4,
+                        gap: '.5rem',
                     },
                     1440: {
                         perPage: 5,
+                        gap: '.5rem',
                     },
-                }
-            });
+                },
+            classes: {
+                arrows: 'splide__arrows custom__arrows',
+                arrow : 'splide__arrow custom__arrow',
+                prev  : 'splide__arrow--prev custom__prev',
+                next  : 'splide__arrow--next custom__next',
+            },
+        });
         splide.mount();
     }
     if(document.querySelector('.accessoires__splide')) {
@@ -26,15 +41,30 @@ document.addEventListener("DOMContentLoaded", function () {
             speed: 1000,
             easing: 'ease',
             perPage: 6,
+            perMove: 1,
+            pagination: false,
+            gap: '.5rem',
                 breakpoints: {
                     767: {
-                        perPage: 1,
+                        perPage: 2,
+                        gap: '.5rem',
+                    },
+                    1024: {
+                        perPage: 4,
+                        gap: '.5rem',
                     },
                     1440: {
                         perPage: 5,
+                        gap: '.5rem',
                     },
-                }
-            });
+                },
+            classes: {
+                arrows: 'splide__arrows custom__arrows',
+                arrow : 'splide__arrow custom__arrow',
+                prev  : 'splide__arrow--prev custom__prev',
+                next  : 'splide__arrow--next custom__next',
+            },
+        });
         splide2.mount();
     }
 })
